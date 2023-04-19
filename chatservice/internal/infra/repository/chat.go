a87c1e0dc69daeb7408bfc63198ac8db40413b18
Luiz Henrique <luizhjramos@outlook.com>
2023-04-19 08:32:12 -0300

    feat: CreateChat repository layer method

---
+++ chatservice/internal/infra/repository/chat.go
@@ -0,0 +1,66 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"time"
+
+	"github.com/MogLuiz/gpt-consumer/chatservice/internal/domain/entity"
+	"github.com/MogLuiz/gpt-consumer/chatservice/internal/infra/db"
+)
+
+type ChatRepositoryMySQL struct {
+	DB      *sql.DB
+	Queries *db.Queries
+}
+
+func NewChatRepositoryMySQL(databaseConection *sql.DB) *ChatRepositoryMySQL {
+	return &ChatRepositoryMySQL{
+		DB:      databaseConection,
+		Queries: db.New(databaseConection),
+	}
+}
+
+func (repository *ChatRepositoryMySQL) CreateChat(ctx context.Context, chat *entity.Chat) error {
+	err := repository.Queries.CreateChat(
+		ctx,
+		db.CreateChatParams{
+			ID:               chat.ID,
+			UserID:           chat.UserID,
+			InitialMessageID: chat.InitialSystemMessage.Content,
+			Status:           chat.Status,
+			TokenUsage:       int32(chat.TokenUsage),
+			Model:            chat.Config.Model.Name,
+			ModelMaxTokens:   int32(chat.Config.Model.MaxTokens),
+			Temperature:      float64(chat.Config.Temperature),
+			TopP:             float64(chat.Config.TopP),
+			N:                int32(chat.Config.N),
+			Stop:             chat.Config.Stop[0],
+			MaxTokens:        int32(chat.Config.MaxTokens),
+			PresencePenalty:  float64(chat.Config.PresencePenalty),
+			FrequencyPenalty: float64(chat.Config.FrequencyPenalty),
+			CreatedAt:        time.Now(),
+			UpdatedAt:        time.Now(),
+		},
+	)
+	if err != nil {
+		return err
+	}
+
+	err = repository.Queries.AddMessage(
+		ctx,
+		db.AddMessageParams{
+			ID:        chat.InitialSystemMessage.ID,
+			ChatID:    chat.ID,
+			Content:   chat.InitialSystemMessage.Content,
+			Role:      chat.InitialSystemMessage.Role,
+			Tokens:    int32(chat.InitialSystemMessage.Tokens),
+			CreatedAt: chat.InitialSystemMessage.CreatedAt,
+		},
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
